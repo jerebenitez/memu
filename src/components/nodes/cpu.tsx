aacc8a6382357d09548a907594040ade310735b1
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-07-29 22:15:55 -0300

    feat: change cpu core name

--- src/components/nodes/cpu.tsx
+++ src/components/nodes/cpu.tsx
@@ -1,14 +1,31 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Handle, type NodeProps, Position } from "@xyflow/react";
-import { Cpu } from "lucide-react";
+import { Cpu, Pencil, Save } from "lucide-react";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import { Input } from "../ui/input";
 
 export function CPUNode({ data }: NodeProps) {
+    const [name, setName] = useState<string>(data.label)
+    const [editing, setEditing] = useState<boolean>(false)
+
+    const onEdit = () => {
+        setEditing(!editing)
+    }
+
     return (
-    <Card className="w-48 shadow-lg">
+    <Card className="w-56 shadow-lg">
       <CardHeader className="pb-2">
-        <CardTitle className="flex items-center gap-2 text-sm">
-          <Cpu className="w-4 h-4" />
-          {data.label}
+      <CardTitle className="flex items-center justify-between text-sm">
+          <div className="flex items-center gap-2">
+            <Cpu className="w-4 h-4" />
+            {!editing ? name : <Input value={name} onClick={e => e.stopPropagation()} onChange={e => setName(e.target.value)} />}
+          </div>
+          <div className="flex gap-1">
+            <Button variant="ghost" size="sm" onClick={onEdit} title="Reset Stats">
+                {!editing ? <Pencil className="w-3 h-3" /> : <Save className="w-3 h-3" />}
+            </Button>
+          </div>
         </CardTitle>
       </CardHeader>
       <CardContent className="pt-0">
